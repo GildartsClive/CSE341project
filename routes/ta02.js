c61d9920b2eaea846d4ed1a2f289d457682e6073
GildartsClive <cmicmuir@gmail.com>
2022-01-13 17:41:44 -0700

    push for week 2: added the code to add and remove users.

--- routes/ta02.js
+++ routes/ta02.js
@@ -3,12 +3,33 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-  res.render('pages', {
-    title: 'Team Activity 02',
-    path: '/ta02', // For pug, EJS
-    activeTA03: true, // For HBS
-    contentCSS: true, // For HBS
+const users = ['Mic', 'Jimmy', 'Ann'];
+
+router.post('/addUser', (req, res, next) => {
+  const newUser = req.body.newUser;
+  users.push(newUser);
+  //console.log(newUser);
+  res.redirect('/ta02/');
+});
+
+router.post('/removeUser', (req, res, next) => {
+  //console.log(req.body.removeUser);
+  const user = req.body.removeUser;
+  
+  const indexNum = users.indexOf(user);
+  if (indexNum !== -1 ) {
+      users.splice(indexNum, 1);
+  }
+  res.redirect('/ta02/');
+});
+
+router.get('/',(req, res, next) => {
+  res.render('pages/ta02', {
+      title: 'Team Activity 02',
+      users: users,
+      path: '/ta02', // For pug, EJS
+      activeTA03: true, // For HBS
+      contentCSS: true, // For HBS
   });
 });
 
